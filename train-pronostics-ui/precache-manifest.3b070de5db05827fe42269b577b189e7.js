6d7195d3e72f3aaeb0234750b897514770c3cf72
sunydev <82972608+sunydev@users.noreply.github.com>
2021-12-23 10:27:47 -0400

    Add files via upload

---
+++ train-pronostics-ui/precache-manifest.3b070de5db05827fe42269b577b189e7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "38af2535079bac9263b7a5c9093e4b5e",
+    "url": "/train-pronostics-ui/index.html"
+  },
+  {
+    "revision": "0e281a0fb25f5a8f63b1",
+    "url": "/train-pronostics-ui/static/css/2.cd26dbc7.chunk.css"
+  },
+  {
+    "revision": "d853af73dc15eebe894e",
+    "url": "/train-pronostics-ui/static/css/main.060aeaaf.chunk.css"
+  },
+  {
+    "revision": "0e281a0fb25f5a8f63b1",
+    "url": "/train-pronostics-ui/static/js/2.9457ac5a.chunk.js"
+  },
+  {
+    "revision": "7ee0693e48d7810c5ff155e5484601ac",
+    "url": "/train-pronostics-ui/static/js/2.9457ac5a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d853af73dc15eebe894e",
+    "url": "/train-pronostics-ui/static/js/main.7c845b0d.chunk.js"
+  },
+  {
+    "revision": "44bc034d111d3e0156e7",
+    "url": "/train-pronostics-ui/static/js/runtime-main.3062e5ad.js"
+  }
+]);
